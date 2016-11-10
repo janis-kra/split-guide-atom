fa11fbf1622ca14ce72da520814789f482ec9247
Janis Krasemann <janis.krasemann@enpit.de>
2016-11-10 23:44:13 +0100

    initial commit

---
+++ spec/split-guide-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SplitGuideAtomView from '../lib/split-guide-atom-view';
+
+describe('SplitGuideAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
