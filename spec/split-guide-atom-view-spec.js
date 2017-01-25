bce86e1b6add8a73b949209b77a831943d2fbc74
Janis Krasemann <janis.krasemann@enpit.de>
2017-01-25 09:02:19 +0100

    implement basic functionality

--- spec/split-guide-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SplitGuideAtomView from '../lib/split-guide-atom-view';
-
-describe('SplitGuideAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
